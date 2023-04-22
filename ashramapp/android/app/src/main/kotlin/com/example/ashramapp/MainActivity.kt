92f8d6ff8e629eb041bc84c037ed225a37309cb7
kirtisikka <kirtisikka972@gmail.com>
2023-04-22 21:22:48 +0530

    start project

---
+++ ashramapp/android/app/src/main/kotlin/com/example/ashramapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ashramapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ashramapp/lib/pages/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  const LoginPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ashram App'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: const Center(
+        child: Text('Login Page'),
+      ),
+    );
+   
+  }
+
+}
