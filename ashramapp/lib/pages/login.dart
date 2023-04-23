acd2eb44bfd8cb25cf0a8e3f336c1c7185a8c739
kirtisikka <kirtisikka972@gmail.com>
2023-04-23 11:46:31 +0530

    sign up page

--- ashramapp/lib/pages/login.dart
+++ ashramapp/lib/pages/login.dart
@@ -6,14 +6,77 @@ class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ashram App'),
-        centerTitle: true,
-        backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
+      body: Column(children: [ 
+        const  SizedBox(height: 150 , width: 100),  
+        Container(
+          alignment: Alignment.center,
+          child :Image.asset('lib/images/logo.jpeg'),
+
+        ),
+  
+        Container(
+  alignment: Alignment.centerLeft,
+  padding: const EdgeInsets.all(22.0),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const Text(
+        'Sign In',
+        style: TextStyle(
+          height: 3,
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
       ),
-      body: const Center(
-        child: Text('Login Page'),
+      const SizedBox(height: 20),
+      TextField(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+        ),
       ),
+      const SizedBox(height: 20),
+      TextField(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+        ),
+      ),
+    ],
+  ),
+),
+
+
+
+
+        
+        
+        
+       
+          
+
+
+
+        
+       
+        
+        // const SizedBox(height: 50),
+        
+      
+        
+      ]) ,
+
+      
     );
    
   }
